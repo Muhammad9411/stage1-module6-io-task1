0ed2d41f3229b775ab2049a399a622b1f47b0513
MuhammadJon <mtjohn9411@gmail.com>
2023-03-29 12:13:55 +0500

    Stage 1 Task IO File uploading solution has been developed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,44 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        try (java.io.FileReader fr = new java.io.FileReader(file)) {
+            String line;
+            BufferedReader reader = new BufferedReader(fr);
+            while ((line = reader.readLine()) != null) {
+                String[] keyValuePair = line.split(":");
+                if (keyValuePair.length > 1) {
+                    String key = keyValuePair[0];
+                    String value = keyValuePair[1];
+                    map.put(key, value.replace(" ", ""));
+                } else {
+                    System.out.println("No Key:Value found in line, ignoring: " + line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Profile profile = new Profile();
+        profile.setName(map.get("Name").toString());
+        profile.setAge(Integer.parseInt(map.get("Age").toString()));
+        profile.setEmail(map.get("Email").toString());
+        profile.setPhone(Long.parseLong(map.get("Phone").toString()));
+
+        return profile;
     }
 }
